9073f22afacdc1bc46e0f9416eda8273214027dc
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-12 20:48:09 -0300

    Adding Input component

--- App.tsx
+++ App.tsx
@@ -12,6 +12,8 @@ import {
 import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
+import { Register } from "./src/screens/Register";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -25,7 +27,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
