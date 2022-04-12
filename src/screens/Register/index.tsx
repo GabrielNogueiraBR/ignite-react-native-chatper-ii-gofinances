2332cc2b00184d1b560060d3e4c92b1d3618192c
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-12 20:56:19 -0300

    Adding Button component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Button } from "../../components/Form/Button";
 
 import { Input } from "../../components/Form/Input";
 
-import { Container, Header, Title, Form } from "./styles";
+import { Container, Header, Title, Form, Fields } from "./styles";
 
 export function Register() {
   return (
@@ -12,9 +13,12 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+        </Fields>
 
-        <Input placeholder="Preço" />
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
