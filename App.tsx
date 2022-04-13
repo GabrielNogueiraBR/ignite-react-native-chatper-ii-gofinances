9f923eb18504cd558bb023668a33119de67a2a19
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-13 20:13:37 -0300

    Creating category select screen

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
 import { Register } from "./src/screens/Register";
+import { CategorySelect } from "./src/screens/CategorySelect";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
   );
 }
