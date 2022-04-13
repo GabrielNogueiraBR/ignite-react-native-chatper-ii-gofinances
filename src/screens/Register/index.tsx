3635ba31c2dd0ced1fa0a50f144b5346e6fc7fed
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-12 21:52:02 -0300

    Creating component to Select category

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../../components/Form/Button";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 
 import { Input } from "../../components/Form/Input";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
@@ -38,6 +39,8 @@ export function Register() {
               isActive={transactionType === 'down'} 
             />
           </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
         <Button title="Enviar" />
