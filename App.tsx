b0bb243ba40aa8230d1a73bbed12cc7a1d73a35c
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-13 21:54:24 -0300

    update: change main screen on App.tsx

--- App.tsx
+++ App.tsx
@@ -13,7 +13,6 @@ import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
 import { Register } from "./src/screens/Register";
-import { CategorySelect } from "./src/screens/CategorySelect";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +27,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CategorySelect />
+      <Register />
     </ThemeProvider>
   );
 }
