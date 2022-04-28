7a8f8289879945e20df53d3f8a9aeb575f69bbd0
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-28 19:36:18 -0300

    chore: React Natigation styles

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Platform } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -8,21 +12,53 @@ import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen 
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          height: 88,
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
         name="Listagem"
         component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
 
-      <Screen 
+      <Screen
         name="Cadastrar"
         component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
       />
 
-      <Screen 
+      <Screen
         name="Resumo"
         component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
       />
     </Navigator>
   );
