62e2d2ffd1ec30d273e22a85583c5268f833f760
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2022-04-28 19:50:00 -0300

    add: react-native-gesture-handler

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -19,6 +19,7 @@ import {
   Transactions,
   TransactionsTitle,
   TransactionList,
+  LogoutButton
 } from "./styles";
 
 export interface DataListProps extends TransactionCardProps {
@@ -78,7 +79,9 @@ export function Dashboard() {
             </User>
           </UserInfo>
 
-          <Icon name="power" />
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
         </UserWrapper>
       </Header>
 
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,6 +2,8 @@ import styled from "styled-components/native";
 import { FlatList, FlatListProps } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { BorderlessButton } from "react-native-gesture-handler";
+
 import {
   getBottomSpace,
   getStatusBarHeight,
@@ -63,6 +65,8 @@ export const UserName = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
+
+export const LogoutButton = styled(BorderlessButton)``;
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
